cbbf676176c9d47237bf0d05b473953cc4a56069
Fayaz <d00217205@student.dkit.ie>
2020-10-21 08:26:07 +0100

    Second Commit

--- src/main/java/dkit/oop/App.java
+++ src/main/java/dkit/oop/App.java
@@ -1,5 +1,7 @@
 package dkit.oop;
 
+import java.sql.SQLOutput;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println();
+        System.out.println("Second commit");
+
+        System.out.println();
+
+        int a = 21,b=24;
+        int c = a+b;
+        System.out.println("c: "+c);
     }
 }
